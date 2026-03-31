3549b354c2da67437699e9578dcf5d6dbea042d9
José Reynoso <reynosotiu@gmail.com>
2026-03-30 19:15:26 -0600

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@
 export const environment = {
   production: true,
   apiUrl: '/api',
-  direccionesApiUrl: '/tiendasgt-direcciones/api',
+  direccionesApiUrl: '/api/direcciones',
   direccionesAdminKey: '1234567890',
   /** Misma raíz pública HTTPS desde la que sirves el admin (para alinear con el enlace del correo de verificación). */
   publicAppUrl: 'https://admin.crunchypaws.tiendasgt.com',
